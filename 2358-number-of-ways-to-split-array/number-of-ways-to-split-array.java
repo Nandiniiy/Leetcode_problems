baaeedf489472b3246d30653409e4d2e46d65686
Nandiniiy <168249043+Nandiniiy@users.noreply.github.com>
2024-06-11 16:44:10 +0530

    Time: 2 ms (100.00%) | Memory: 58 MB (74.66%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
@@ -1,23 +1,25 @@
 class Solution {
     public int waysToSplitArray(int[] nums) {
-        //create a prefix array
-        long prefix[] =new long[nums.length];
-        prefix[0] = nums[0];
-        for( int i=1; i<nums.length; i++)
-        {
-            prefix[i] = prefix[i-1] + nums[i];
-        }
-        int countValidsplits = 0;
-        for(int i =0; i<nums.length-1; i++)
-        {
-            long leftpart = prefix[i];
-            long rightpart = prefix[prefix.length-1] - prefix[i];
-            if(leftpart >= rightpart)
-            {
-                countValidsplits++;
-            }
+       long entiresum = 0;
+       int count =0;
+       for(int i : nums)
+       {
+        entiresum = entiresum + i;
 
+       }
+       //now remove the current everytime from the entire sum
+       //and store the curretn sum in the left sum
+       long leftsum = 0;
+       for(int i=0; i<nums.length-1; i++)
+       {
+        leftsum = leftsum + nums[i];
+        entiresum = entiresum - nums[i];
+        if(leftsum>= entiresum)
+        {
+            count++;
         }
-        return countValidsplits;
+
+       }
+       return count;
     }
 }
