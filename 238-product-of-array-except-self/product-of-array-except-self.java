95737dad93507379780256cbc84f8913ff4d143b
Nandiniiy <168249043+Nandiniiy@users.noreply.github.com>
2024-06-13 17:13:35 +0530

    Time: 2 ms (88.74%) | Memory: 55.2 MB (60.45%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -3,22 +3,38 @@ class Solution {
         //computer prefix product for left to right product
         //computer suffix product for right to left product
         //finally the answer left and right product
-        int left[] = new int[nums.length]; //prefix product
-        int right[] = new int[nums.length]; //suffix product
+        //approach 1- prefic suffix
+        //int left[] = new int[nums.length]; //prefix product
+        //int right[] = new int[nums.length]; //suffix product
+        //int finalans[] = new int[nums.length];
+        //left[0] = 1;
+        //for(int i=1; i<nums.length; i++)
+        //{
+          //  left[i] = left[i-1] * nums[i-1];
+        //}
+        //right[right.length-1] = 1;
+        //for(int i=right.length-2; i>=0; i--)
+        //{
+       //     right[i] = right[i+1] * nums[i+1];
+        //}
+       // for(int i=0; i<finalans.length;i++)
+       // {
+      //      finalans[i] = left[i] * right[i];
+        //}
+        //return finalans;
+
         int finalans[] = new int[nums.length];
-        left[0] = 1;
-        for(int i=1; i<nums.length; i++)
+        finalans[0] =1;
+        for(int i=1; i<finalans.length; i++)
         {
-            left[i] = left[i-1] * nums[i-1];
+            finalans[i] = finalans[i-1] * nums[i-1];
         }
-        right[right.length-1] = 1;
-        for(int i=right.length-2; i>=0; i--)
+        //computer suffix product
+        int right =1;
+        for(int i= finalans.length-1; i>=0; i--)
         {
-            right[i] = right[i+1] * nums[i+1];
-        }
-        for(int i=0; i<finalans.length;i++)
-        {
-            finalans[i] = left[i] * right[i];
+            finalans[i] = finalans[i] * right;
+            right = right * nums[i];
         }
         return finalans;
     }
