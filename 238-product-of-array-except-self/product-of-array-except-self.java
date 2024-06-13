686f9b628cdce46382970760c694f7b4069f80ee
Nandiniiy <168249043+Nandiniiy@users.noreply.github.com>
2024-06-13 16:57:50 +0530

    Time: 2 ms (88.74%) | Memory: 55.2 MB (60.45%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        //computer prefix product for left to right product
+        //computer suffix product for right to left product
+        //finally the answer left and right product
+        int left[] = new int[nums.length]; //prefix product
+        int right[] = new int[nums.length]; //suffix product
+        int finalans[] = new int[nums.length];
+        left[0] = 1;
+        for(int i=1; i<nums.length; i++)
+        {
+            left[i] = left[i-1] * nums[i-1];
+        }
+        right[right.length-1] = 1;
+        for(int i=right.length-2; i>=0; i--)
+        {
+            right[i] = right[i+1] * nums[i+1];
+        }
+        for(int i=0; i<finalans.length;i++)
+        {
+            finalans[i] = left[i] * right[i];
+        }
+        return finalans;
+    }
+}
