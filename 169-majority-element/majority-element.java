ac8e77a701568fefb6bc6cf30aa9a777d35e3ddf
Nandiniiy <168249043+Nandiniiy@users.noreply.github.com>
2024-06-13 16:04:51 +0530

    Time: 3 ms (61.76%) | Memory: 53.1 MB (30.26%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,23 +1,7 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        //hashing
-        //count freuqncy, and if it is greater than n/2 return it
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for( int val: nums)
-        {
-            Integer count = map.get(val);
-            if(count!=null)
-            {
-                count++;
-                map.put(val, count);
-            }
-            else
-            { count=1;
-                map.put(val,1);
-            }
-            if(count> nums.length/2)
-            return val;
-        }
-        return -1;
+       Arrays.sort(nums);
+       return nums[nums.length/2];
+
     }
 }
