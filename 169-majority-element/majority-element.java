9d6a7af7dff38c0b7502ed24b556915427d42b0f
Nandiniiy <168249043+Nandiniiy@users.noreply.github.com>
2024-06-13 16:25:56 +0530

    Time: 1 ms (99.81%) | Memory: 53 MB (30.26%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,7 +1,30 @@
 class Solution {
     public int majorityElement(int[] nums) {
-       Arrays.sort(nums);
-       return nums[nums.length/2];
+      //approach 3  
+       //Arrays.sort(nums);
+       //return nums[nums.length/2];
+       
+       //approach 4
+       //Boyer moore voting method
+       int me=0;
+       int count =0;
+       for(int val : nums)
+       {
+        if(count ==0)
+        {
+            me=val;
+            count++;
+        }
+        else if(me== val)
+        {
+          count++;
+        }
+        else
+        {
+            count--;
 
+        }
+       }
+       return me;
     }
 }
